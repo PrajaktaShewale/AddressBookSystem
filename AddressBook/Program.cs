c9fa74b16f51cc3fc35e2014acd04cdaa5d1d686
PrajaktaShewale <shewalep94@gmail.com>
2022-11-07 13:57:53 +0530

    [Prajakta]Add:ContactDetails

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacts contact = new Contacts()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNumber = Console.ReadLine(),
+                Email = Console.ReadLine()
+            };
+            Console.WriteLine("contact Details"  + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.
+                City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    }
+}
